f95f9f46593c5bed05c6e5c441503f68776fd3b0
xuguoming <xuguoming1997@gmail.com>
2017-03-16 23:08:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xuguoming/link/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.xuguoming.link;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.xuguoming.link.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
